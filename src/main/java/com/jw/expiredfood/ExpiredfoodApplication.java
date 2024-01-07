62f3059786e23ba2f02be8d4eea48ac876781aa5
LAPTOP-CLI1VKRV\wen82 <wen821107@gmail.com>
2024-01-07 18:11:29 +0800

    spring init

---
+++ src/main/java/com/jw/expiredfood/ExpiredfoodApplication.java
@@ -0,0 +1,13 @@
+package com.jw.expiredfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpiredfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpiredfoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jw/expiredfood/ExpiredfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jw.expiredfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpiredfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
